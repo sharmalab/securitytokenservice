4345baaafc1fbb5f5f1476ad804413d493601d85
nadirsaghar <nadirsaghar@yahoo.com>
2013-05-17 16:50:16 -0400

    updated pom file and project

---
+++ source/projects/sts-junit/src/main/java/edu/emory/cci/bindaas/sts/junit/exception/JunitRunnerException.java
@@ -0,0 +1,30 @@
+package edu.emory.cci.bindaas.sts.junit.exception;
+
+public class JunitRunnerException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public JunitRunnerException() {
+		super();
+		
+	}
+
+	public JunitRunnerException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		
+	}
+
+	public JunitRunnerException(String arg0) {
+		super(arg0);
+		
+	}
+
+	public JunitRunnerException(Throwable arg0) {
+		super(arg0);
+		
+	}
+
+}
