c07caca2b2f66cd4126a514183201b75a4488e75
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2019-06-02 18:57:10 -0400

    Add sts-junit

---
+++ projects/sts-junit/src/main/java/org/rakshak/junit/exception/JunitRunnerException.java
@@ -0,0 +1,30 @@
+package org.rakshak.junit.exception;
+
+public class JunitRunnerException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public JunitRunnerException() {
+		super();
+		
+	}
+
+	public JunitRunnerException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		
+	}
+
+	public JunitRunnerException(String arg0) {
+		super(arg0);
+		
+	}
+
+	public JunitRunnerException(Throwable arg0) {
+		super(arg0);
+		
+	}
+
+}
